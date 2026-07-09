8fc51f357b3287c0d7e6f1e12a597899d7796868
mahesh <>
2026-07-09 23:30:22 +0530

    update

--- src/main/java/com/ermapsh/razorpay/common/enums/ApiResponse.java
+++ src/main/java/com/ermapsh/razorpay/common/enums/ApiResponse.java
@@ -5,9 +5,15 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import java.time.LocalDateTime;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
+
 public record ApiResponse<T>(
         Integer code,
         String message,
         T data,
-        Object error
-) { }
+        Object error,
+        LocalDateTime timestamp
+) {
+    public ApiResponse(Integer code, String message, T data, Object error) {
+        this(code, message, data, error, LocalDateTime.now());
+    }
+}
--- src/main/java/com/ermapsh/razorpay/controller/HealthCheckController.java
+++ src/main/java/com/ermapsh/razorpay/controller/HealthCheckController.java
@@ -6,9 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
-
-
 @RestController
 public class HealthCheckController {
 
@@ -18,7 +15,6 @@ public class HealthCheckController {
                 new ApiResponse(
                         HttpStatus.OK.value(),
                         "Live",
-//                        LocalDateTime.now(),
                         null,
                         null
                 )
